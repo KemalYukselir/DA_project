3a88d33eb17c201887188b126173f96b73bc267c
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-04-22 20:13:13 +0100

    renamed import filename

---
+++ import_df.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+
+def get_dataframe():
+    """
+    Import appendix CSV file into a pandas DataFrame.
+    Returns:
+        pd.DataFrame: The imported DataFrame.
+    """
+
+    df = pd.read_csv('appendix.csv')
+
+    # Ensure all columns are displayed
+    pd.set_option('display.max_columns', None)
+
+    return df
