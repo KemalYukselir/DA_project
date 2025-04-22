14d0432d98bd542be4bab0a931d88e4d6200aee2
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-04-22 20:33:10 +0100

    unique object value check with notes

--- eda.py
+++ eda.py
@@ -10,3 +10,29 @@ df = get_dataframe()
 print(df.info())
 # Seems to be no nulls 
 # We will look at object data types
+
+##################
+## Check unique values in columns 
+##################
+
+# Quick way to check values in all columns
+def check_uniques_in_objects(df):
+  for col in df.columns:
+    if df[col].dtype == 'object':
+      print("\n\n")
+      print(f'{col}: {df[col].unique()}')
+      print("\n\n")
+
+check_uniques_in_objects(df)
+
+
+# Notes
+"""
+- Institution seems fine
+- Course Number seems fine
+- Launch date needs to be converted to a format
+- Course Title seems fine
+- Instructors seems fine
+- Course Subject seems fine
+- Played video has --- ?? - can aslo be converted to a float
+"""
