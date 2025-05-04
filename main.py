980899b28cb0909d31751270e2dce32f4953d821
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-04 14:21:06 +0100

    removed input type. Changed title from readme to overview

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ model = load_model()
 
 # Sidebar for navigation
 page = st.sidebar.radio("📂 Select a Page", ["Project Overview","Ethical Standards","Insights" ,"Predictor"])
-mode = st.sidebar.selectbox("Select Input Mode:", ["Manual Input"])
 
 def project_overview_page():
-  st.title("📘 Project README 📘")
+  st.title("📘 Project Overview 📘")
 
   st.markdown("""
   ## 🎓 Course Certification Rates Predictor 🎓
@@ -149,41 +148,40 @@ def model_page():
     # Title
     st.title("🎓 Student Certification Rate Predictor")
     # Manual input mode
-    if mode == "Manual Input":
-        st.subheader("Enter Course Details")
+    st.subheader("Enter Course Details")
 
-        course_subject = st.selectbox("Course Subject", [
-            "Government, Health, and Social Science",
-            "Humanities, History, Design, Religion, and Education",
-            "Science, Technology, Engineering, and Mathematics",
-            'Computer Science'
-        ])
+    course_subject = st.selectbox("Course Subject", [
+        "Government, Health, and Social Science",
+        "Humanities, History, Design, Religion, and Education",
+        "Science, Technology, Engineering, and Mathematics",
+        'Computer Science'
+    ])
 
-        particapants = st.slider("Number of Participants", 0, 10000, 5000)
-        total_course_hours = st.slider("Total Course Hours", 0.0, 1000.0, 418.94)
-        percent_male = st.slider("% Male Participants In Course", 0.0, 100.0, 88.28)
-        median_age = st.slider("Median Age of Participants", 0.0, 100.0, 26.0)
-        percent_bachelor_degree = st.slider("% Participants With Bachelor's Degree or Higher", 0.0, 100.0, 50.0)
-        percent_grade_higher = st.slider("% Participants With Grade Higher Than Zero From Quizes", 0.0, 100.0, 28.97)
-        audited_50plus = st.slider("Number of Audited Participants (> 50% Course Content Accessed)", 0, 10000, 5000)
-        percent_certified_50plus = st.slider("% Certified of > 50% Course Content Accessed", 0.0, 100.0, 54.98)
+    particapants = st.slider("Number of Participants", 0, 10000, 5000)
+    total_course_hours = st.slider("Total Course Hours", 0.0, 1000.0, 418.94)
+    percent_male = st.slider("% Male Participants In Course", 0.0, 100.0, 88.28)
+    median_age = st.slider("Median Age of Participants", 0.0, 100.0, 26.0)
+    percent_bachelor_degree = st.slider("% Participants With Bachelor's Degree or Higher", 0.0, 100.0, 50.0)
+    percent_grade_higher = st.slider("% Participants With Grade Higher Than Zero From Quizes", 0.0, 100.0, 28.97)
+    audited_50plus = st.slider("Number of Audited Participants (> 50% Course Content Accessed)", 0, 10000, 5000)
+    percent_certified_50plus = st.slider("% Certified of > 50% Course Content Accessed", 0.0, 100.0, 54.98)
 
-        # Build input data dictionary
-        input_data = {
-            "const": 1,
-            "% Certified of > 50% Course Content Accessed": percent_certified_50plus,
-            "% Grade Higher Than Zero": percent_grade_higher,
-            "Total Course Hours (Thousands)": total_course_hours,
-            "Median Age": median_age,
-            "% Male": percent_male,
-            "Course Subject": course_subject,
-            "% Bachelor's Degree or Higher": percent_bachelor_degree,
-            "% Deep learners": (audited_50plus / particapants) * 100
-        }
+    # Build input data dictionary
+    input_data = {
+        "const": 1,
+        "% Certified of > 50% Course Content Accessed": percent_certified_50plus,
+        "% Grade Higher Than Zero": percent_grade_higher,
+        "Total Course Hours (Thousands)": total_course_hours,
+        "Median Age": median_age,
+        "% Male": percent_male,
+        "Course Subject": course_subject,
+        "% Bachelor's Degree or Higher": percent_bachelor_degree,
+        "% Deep learners": (audited_50plus / particapants) * 100
+    }
 
-        if st.button("Predict % Certified"):
-            prediction = model.predict_from_model(input_data)
-            st.success(f"Predicted Certification Rate: {prediction.iloc[0]:.2f}%")  
+    if st.button("Predict % Certified"):
+        prediction = model.predict_from_model(input_data)
+        st.success(f"Predicted Certification Rate: {prediction.iloc[0]:.2f}%")  
    
 
 
